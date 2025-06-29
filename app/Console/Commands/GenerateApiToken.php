7f8be59bf20c946c8319357ce7ebf2a7b9f17718
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-06-29 21:34:39 +0300

    создание команды для генерации токена

---
+++ app/Console/Commands/GenerateApiToken.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\DTOs\ExternalSourceDto;
+use App\Models\ExternalSource;
+use App\Models\ExternalSourceAccessTokens;
+use App\Services\External\Source\ExternalSourceService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\Exception;
+
+class GenerateApiToken extends Command
+{
+    protected $signature = 'app:generate-token {source} {hook_url}';
+
+    protected $description = 'Генерирует токен Sanctum для пользователя, создаёт пользователя если нет';
+
+    public function handle(): int
+    {
+        try {
+            $sourceName = $this->argument('source');
+            $hookUrl = $this->argument('hook_url');
+
+            $validator = Validator::make([
+                'source' => $sourceName,
+                'hook_url' => $hookUrl,
+            ], [
+                'source' => ['required', 'string', 'min:3', 'max:100'],
+                'hook_url' => ['required', 'url'],
+            ]);
+
+            if ($validator->fails()) {
+                foreach ($validator->errors()->all() as $error) {
+                    $this->error($error);
+                }
+                return 1;
+            }
+
+            DB::transaction(function () use ($sourceName, $hookUrl) {
+                $externalSourceData = [
+                    'name' => $sourceName,
+                    'webhook_url' => $hookUrl,
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ];
+
+                $sourceItem = ExternalSource::where('name', $sourceName)->first();
+                if (!$sourceItem) {
+                    $this->info('Добавляем новый ресурс...');
+
+                    $sourceData = ExternalSourceDto::from(array_merge($externalSourceData, [
+                        'created_at' => date('Y-m-d H:i:s'),
+                    ]));
+
+                    $sourceItem = (new ExternalSourceService())->create($sourceData);
+                } else {
+                    $this->info("Обновляем ресурс {$sourceName}...");
+
+                    $sourceData = ExternalSourceDto::from(array_merge($externalSourceData, [
+                        'id' => $sourceItem->id,
+                        'created_at' => date('Y-m-d H:i:s'),
+                    ]));
+
+                    (new ExternalSourceService())->update($sourceData);
+                }
+
+                $accessToken = (new ExternalSourceAccessTokens())
+                    ->where('external_source_id', $sourceItem->id)
+                    ->first();
+
+                if (!$accessToken) {
+                    throw new Exception('Токен не создан!');
+                }
+
+                $this->info("Токен успешно сгенерирован! {$sourceItem->name} : {$accessToken->token}");
+            });
+
+            return 0;
+        } catch (\Exception $exception) {
+            $this->error("Не удалось добавить ресурс: {$exception->getMessage()}");
+            return 1;
+        }
+    }
+}
