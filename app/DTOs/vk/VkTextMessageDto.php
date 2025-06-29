c1151fa9888698f7c7c5846997f74680acb4af5f
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-06-29 21:27:08 +0300

    настройка dto для vk

--- app/DTOs/vk/VkTextMessageDto.php
+++ app/DTOs/vk/VkTextMessageDto.php
@@ -4,26 +4,8 @@ namespace App\DTOs\VK;
 
 use Spatie\LaravelData\Data;
 
-/**
- * Отправка сообщения в VK
- *
- * @property string $methodQuery
- * @property string|null $title
- * @property string|null $file
- * @property int|null $peer_id
- * @property string|null $message
- * @property string|null $attachment
- */
 class VkTextMessageDto extends Data
 {
-    /**
-     * @param string $methodQuery
-     * @param string|null $title
-     * @param string|null $file
-     * @param int|null $peer_id
-     * @param string|null $message
-     * @param string|null $attachment
-     */
     public function __construct(
         public string $methodQuery,
         public ?string $title,
@@ -31,14 +13,15 @@ class VkTextMessageDto extends Data
         public ?int $peer_id,
         public ?string $message,
         public ?string $attachment
-    ) {}
+    ) {
+    }
 
     /**
      * @return array
      */
     public function toArray(): array
     {
-        $dataMessage = array_filter(parent::toArray(), fn($value) => !is_null($value));
+        $dataMessage = array_filter(parent::toArray(), fn ($value) => !is_null($value));
         unset($dataMessage['methodQuery']);
         return $dataMessage;
     }
--- app/DTOs/vk/VkUpdateDto.php
+++ app/DTOs/vk/VkUpdateDto.php
@@ -3,37 +3,10 @@
 namespace App\DTOs\VK;
 
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Exception;
 
-/**
- * DTO для запроса из VK
- *
- * @property string|null $secret
- * @property int $group_id
- * @property string $type
- * @property string $event_id
- * @property string $v
- * @property int $from_id
- * @property int $id
- * @property string|null $text
- * @property array|null $geo
- * @property array $rawData
- * @property array $listFileUrl
- */
 readonly class VkUpdateDto
 {
-    /**
-     * @param string|null $secret
-     * @param int $group_id
-     * @param string $type
-     * @param string $event_id
-     * @param string $v
-     * @param int $from_id
-     * @param int $id
-     * @param string|null $text
-     * @param array|null $geo
-     * @param array $rawData
-     * @param array $listFileUrl
-     */
     public function __construct(
         public ?string $secret,
         public int $group_id,
@@ -46,19 +19,20 @@ readonly class VkUpdateDto
         public ?array $geo,
         public array $rawData,
         public array $listFileUrl
-    ) {}
+    ) {
+    }
 
-    /**
-     * @param Request $request
-     * @return self|null
-     */
     public static function fromRequest(Request $request): ?self
     {
         try {
             $data = $request->all();
 
+            if (empty($data)) {
+                throw new Exception('Контекст не найден');
+            }
+
             return new self(
-                secret: $data['secret'] ?? "",
+                secret: $data['secret'] ?? '',
                 group_id: $data['group_id'],
                 type: $data['type'],
                 event_id: $data['event_id'],
@@ -77,38 +51,36 @@ readonly class VkUpdateDto
 
     /**
      * Get attachments
+     *
      * @param array $attachments
+     *
      * @return array
      */
     private static function getListUrlAttachments(array $attachments): array
     {
-        try {
-            if (empty($attachments)) {
-                return [];
-            }
-
-            $result = [];
-            foreach ($attachments as $attachment) {
-                $attachmentData = $attachment[$attachment['type']];
-                switch ($attachment['type']) {
-                    case 'photo':
-                        $result[] = $attachmentData['orig_photo']['url'];
-                        break;
-
-                    case 'doc':
-                    case 'graffiti':
-                        $result[] = $attachmentData['url'];
-                        break;
-
-                    case 'audio_message':
-                        $result[] = $attachmentData['link_mp3'];
-                        break;
-                }
-            }
-
-            return $result;
-        } catch (\Exception $e) {
+        if (empty($attachments)) {
             return [];
         }
+
+        $result = [];
+        foreach ($attachments as $attachment) {
+            $attachmentData = $attachment[$attachment['type']];
+            switch ($attachment['type']) {
+                case 'photo':
+                    $result[] = $attachmentData['orig_photo']['url'];
+                    break;
+
+                case 'doc':
+                case 'graffiti':
+                    $result[] = $attachmentData['url'];
+                    break;
+
+                case 'audio_message':
+                    $result[] = $attachmentData['link_mp3'];
+                    break;
+            }
+        }
+
+        return $result;
     }
 }
