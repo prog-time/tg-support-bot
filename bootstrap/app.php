e6da09d17163a30671c26c3e94d09a1dcb799ca5
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-07-22 18:04:45 +0300

    интегрировал sentry в проект

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Sentry\Laravel\Integration;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -19,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
+
         $exceptions->render(function (RouteNotFoundException $e, Request $request) {
             return response()->json([
                 'message' => 'Route not found.',
