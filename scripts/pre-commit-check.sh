f0d5907e4c67c8b1ae6fe1a966fa11b8fb07306e
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-08-19 17:11:40 +0300

    настройка конфигурации сборки

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-bash scripts/check_scripts/check_pint.sh commit
-bash scripts/check_scripts/check_phpstan.sh commit
-bash scripts/check_scripts/check_swagger.sh commit
+#bash scripts/check_scripts/check_pint.sh commit
+#bash scripts/check_scripts/check_phpstan.sh commit
+#bash scripts/check_scripts/check_swagger.sh commit
