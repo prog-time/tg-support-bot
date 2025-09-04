00950fcaa5eba30f108dc203b8c809c14addc269
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-04 20:20:05 +0300

    добавил контроллер SimplePage

---
+++ app/Http/Controllers/SimplePage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+
+class SimplePage
+{
+    /**
+     * @return View
+     */
+    public function index(): View
+    {
+        if (config('app.url') === 'https://tg-support-bot.ru/') {
+            return view('site.home');
+        } else {
+            return view('site.home_client_version');
+        }
+    }
+}
