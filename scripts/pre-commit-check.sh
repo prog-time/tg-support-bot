f85a9fb82b1764ac53f9784bb00f034da56252c2
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-13 00:47:41 +0300

    настройка конфигурации и скриптов для линтера

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -2,6 +2,17 @@
 
 set -e
 
-#bash scripts/check_scripts/check_pint.sh commit
-#bash scripts/check_scripts/check_phpstan.sh commit
-#bash scripts/check_scripts/check_swagger.sh commit
+# проверка наличия тестов
+echo "🔍 [1/4] Проверка наличия тестов..."
+bash scripts/check_scripts/find_test.sh commit
+echo
+
+# исправления стиля кода
+echo "🎨 [2/3] Исправление стиля кода (Pint)..."
+bash scripts/check_scripts/check_pint.sh commit
+echo
+
+# проверка на наличие ошибок
+echo "🧪 [3/3] Проверка типизации (PHPStan)..."
+bash scripts/check_scripts/check_phpstan.sh commit
+echo
