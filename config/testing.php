0b6bf1e5bd8da97cccab54c07abcc6d522b55468
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-13 00:47:41 +0300

    настройка конфигурации и скриптов для линтера

---
+++ config/testing.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'tg_private' => [
+        'chat_id' => env('TEST_USER_CHAT_ID', ''),
+        'username' => env('TEST_USER_USERNAME', ''),
+        'first_name' => env('TEST_USER_FIRST_NAME', ''),
+        'last_name' => env('TEST_USER_LAST_NAME', ''),
+    ],
+];
--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-# bash scripts/check_scripts/check_swagger.sh push
+echo "🧪 [1/1] Генерация Swagger"
+bash scripts/check_scripts/check_swagger.sh commit
+echo
