21c006f8fe083a6e7b4a296f6aeb0b1d87c668e5
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-16 17:18:34 +0300

    поправил UploadFileVkTest и создал тест

--- app/DTOs/vk/VkAnswerDto.php
+++ app/DTOs/vk/VkAnswerDto.php
@@ -1,23 +1,56 @@
 <?php
 
-namespace App\DTOs\VK;
+namespace App\DTOs\Vk;
+
+use App\Enums\VkError;
 
 readonly class VkAnswerDto
 {
     public function __construct(
-        public ?array $error,
+        public int $response_code,
+        public ?string $error_type,
+        public ?string $error_message,
         public int|array $response,
-    ) {}
+    ) {
+    }
 
     /**
      * @param array $dataAnswer
+     *
      * @return self
      */
     public static function fromData(array $dataAnswer): self
     {
+        if (empty($dataAnswer['response_code'])) {
+            $dataAnswer['response_code'] = 200;
+        }
+
+        if (!empty($dataAnswer['error_message'])) {
+            $typeError = self::exactTypeError($dataAnswer['error_message']);
+        }
+
         return new self(
-            error: $dataAnswer['error'] ?? null,
+            response_code: $dataAnswer['response_code'],
+            error_type: $typeError ?? null,
+            error_message: $dataAnswer['error_message'] ?? null,
             response: $dataAnswer['response'],
         );
+    }
+
+    /**
+     * Получение кода ошибки
+     *
+     * @param string $textError
+     *
+     * @return string|null
+     */
+    private static function exactTypeError(string $textError): ?string
+    {
+        try {
+            $error = VkError::fromResponse($textError);
+            return $error->name;
+        } catch (\Throwable $e) {
+            return null;
+        }
     }
 }
