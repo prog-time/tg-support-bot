69bfc561ae6ceda18f5438deaf8a22c697e3f121
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-16 17:23:50 +0300

    добавил testing конфиг

--- config/testing.php
+++ config/testing.php
@@ -7,4 +7,7 @@ return [
         'first_name' => env('TEST_USER_FIRST_NAME', ''),
         'last_name' => env('TEST_USER_LAST_NAME', ''),
     ],
+    'vk_private' => [
+        'chat_id' => env('TEST_VK_USER_CHAT_ID', ''),
+    ],
 ];
