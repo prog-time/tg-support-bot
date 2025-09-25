6869eb6f302251f74ed56c72ff656f30fbbedc89
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-09-25 23:15:32 +0300

    поправил конфиг

--- config/testing.php
+++ config/testing.php
@@ -10,6 +10,9 @@ return [
         'first_name' => env('TEST_USER_FIRST_NAME', ''),
         'last_name' => env('TEST_USER_LAST_NAME', ''),
     ],
+    'tg_group' => [
+        'chat_id' => env('TELEGRAM_GROUP_ID', ''),
+    ],
     'tg_private' => [
         'chat_id' => env('TEST_USER_CHAT_ID', ''),
         'username' => env('TEST_USER_USERNAME', ''),
@@ -20,8 +23,10 @@ return [
         'chat_id' => env('TEST_VK_USER_CHAT_ID', ''),
     ],
     'tg_file' => [
-        'document' => env('TEST_PHOTO', ''),
-        'photo' => env('TEST_DOCUMENT', ''),
+        'document' => env('TEST_DOCUMENT', ''),
+        'photo' => env('TEST_PHOTO', ''),
         'sticker' => env('TEST_STICKER', ''),
+        'video_note' => env('TEST_VIDEO_NOTE', ''),
+        'voice' => env('TEST_VOICE', ''),
     ],
 ];
