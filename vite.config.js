9fbd027d2cf8f529dd152145748219e6fb3319d7
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-10-10 12:09:39 +0300

    настройка виджета живого чата

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,9 +6,25 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'public/live_chat/widget.js', // виджет
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                widget: 'public/live_chat/widget.js', // наш виджет
+            },
+            output: {
+                entryFileNames: 'widget.js', // фиксированное имя
+            },
+        },
+        outDir: 'public/live_chat/dist', // кладём прямо в dist
+        emptyOutDir: false,               // чтобы не удалять другие файлы
+    },
 });
