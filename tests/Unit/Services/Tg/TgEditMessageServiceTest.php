26f2be44acc1d52517823cdee5e56127dbfb2c59
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-11-13 01:51:35 +0300

    поправил TgEditMessageServiceTest
    ------------------------------
    Изменённые файлы:
    Изменён:    tests/Unit/Services/Tg/TgEditMessageServiceTest.php

--- tests/Unit/Services/Tg/TgEditMessageServiceTest.php
+++ tests/Unit/Services/Tg/TgEditMessageServiceTest.php
@@ -2,17 +2,16 @@
 
 namespace Tests\Unit\Services\Tg;
 
-use App\DTOs\TelegramUpdateDto;
 use App\Models\BotUser;
 use App\Models\Message;
 use App\Services\Tg\TgEditMessageService;
 use App\Services\Tg\TgMessageService;
-use Illuminate\Support\Facades\Request;
+use Tests\Mocks\Tg\TelegramUpdateDtoMock;
 use Tests\TestCase;
 
 class TgEditMessageServiceTest extends TestCase
 {
-    private array $basicPayload;
+    private ?BotUser $botUser;
 
     public function setUp(): void
     {
@@ -20,81 +19,48 @@ class TgEditMessageServiceTest extends TestCase
 
         Message::truncate();
 
-        $this->basicPayload = [
-            'message_id' => time(),
-            'from' => [
-                'id' => config('testing.tg_private.chat_id'),
-                'is_bot' => false,
-                'first_name' => config('testing.tg_private.first_name'),
-                'last_name' => config('testing.tg_private.last_name'),
-                'username' => config('testing.tg_private.username'),
-                'language_code' => 'ru',
-            ],
-            'chat' => [
-                'id' => config('testing.tg_private.chat_id'),
-                'first_name' => config('testing.tg_private.first_name'),
-                'last_name' => config('testing.tg_private.last_name'),
-                'username' => config('testing.tg_private.username'),
-                'type' => 'private',
-            ],
-            'date' => time(),
-            'edit_date' => time(),
-            'text' => 'Тестовое сообщение',
-        ];
+        $this->botUser = BotUser::getUserByChatId(config('testing.tg_private.chat_id'), 'telegram');
     }
 
     public function test_edit_text_message(): void
     {
-        $chatId = config('testing.tg_private.chat_id');
-        $botUser = BotUser::getUserByChatId($chatId, 'telegram');
+        Message::truncate();
 
         // новое сообщение
-        $newMessageDto = TelegramUpdateDto::fromRequest(
-            Request::create('api/telegram/bot', 'POST', [
-                'update_id' => time(),
-                'message' => $this->basicPayload,
-            ])
-        );
+        $newMessageDto = TelegramUpdateDtoMock::getDto();
         (new TgMessageService($newMessageDto))->handleUpdate();
 
         // Проверяем, что сообщение сохранилось в базе
-        $this->assertDatabaseHas('messages', [
-            'bot_user_id' => $botUser->id,
+        $whereMessageParams = [
+            'bot_user_id' => $this->botUser->id,
             'message_type' => 'incoming',
             'platform' => 'telegram',
-        ]);
+        ];
+        $this->assertDatabaseHas('messages', $whereMessageParams);
 
-        $newMessageData = Message::where('bot_user_id', $botUser->id)
-            ->orderBy('id', 'desc')
-            ->first();
+        $newMessageData = Message::where($whereMessageParams)->orderBy('id', 'desc')->first();
         // ---------------
 
         // изменение сообщения
         $editPayload = [
             'update_id' => time(),
-            'edited_message' => $this->basicPayload,
+            'edited_message' => TelegramUpdateDtoMock::getDtoParams()['message'],
         ];
 
         $editPayload['edited_message']['text'] = 'Новый текст сообщения';
         $editPayload['edited_message']['message_id'] = $newMessageData->from_id;
-        $editPayload['edited_message']['chat']['id'] = $botUser->chat_id;
-        $editPayload['edited_message']['message_thread_id'] = $botUser->topic_id;
+        $editPayload['edited_message']['chat']['id'] = $this->botUser->chat_id;
+        $editPayload['edited_message']['message_thread_id'] = $this->botUser->topic_id;
 
-        $newMessageDto = TelegramUpdateDto::fromRequest(
-            Request::create('api/telegram/bot', 'POST', $editPayload)
-        );
-        (new TgEditMessageService($newMessageDto))->handleUpdate();
-
-        $this->app->make('queue')->connection('sync');
+        $editMessageDto = TelegramUpdateDtoMock::getDto($editPayload);
+        (new TgEditMessageService($editMessageDto))->handleUpdate();
     }
 
     public function test_edit_caption_message(): void
     {
-        $chatId = config('testing.tg_private.chat_id');
-        $botUser = BotUser::getUserByChatId($chatId, 'telegram');
+        Message::truncate();
 
-        $basicPayload = $this->basicPayload;
-        $basicPayload['photo'] = [
+        $photoData = [
             [
                 'file_id' => config('testing.tg_file.photo'),
                 'file_unique_id' => 'AQAD854DoEp9',
@@ -105,48 +71,41 @@ class TgEditMessageServiceTest extends TestCase
         ];
 
         // новое сообщение
-        $newMessagePayload = [
-            'update_id' => time(),
-            'message' => $basicPayload,
-        ];
-        $newMessagePayload['message']['caption'] = $newMessagePayload['message']['text'];
+        $newMessagePayload = TelegramUpdateDtoMock::getDtoParams();
         unset($newMessagePayload['message']['text']);
 
+        $newMessagePayload['message']['photo'] = $photoData;
+        $newMessagePayload['message']['caption'] = 'Версия 1';
 
-        $newMessageDto = TelegramUpdateDto::fromRequest(
-            Request::create('api/telegram/bot', 'POST', $newMessagePayload)
-        );
+        $newMessageDto = TelegramUpdateDtoMock::getDto($newMessagePayload);
         (new TgMessageService($newMessageDto))->handleUpdate();
 
         // Проверяем, что сообщение сохранилось в базе
-        $this->assertDatabaseHas('messages', [
-            'bot_user_id' => $botUser->id,
+        $whereMessageParams = [
+            'bot_user_id' => $this->botUser->id,
             'message_type' => 'incoming',
             'platform' => 'telegram',
-        ]);
+        ];
+        $this->assertDatabaseHas('messages', $whereMessageParams);
 
-        $newMessageData = Message::where('bot_user_id', $botUser->id)
-            ->orderBy('id', 'desc')
-            ->first();
+        $newMessageData = Message::where($whereMessageParams)->orderBy('id', 'desc')->first();
         // ---------------
 
         // изменение сообщения
         $editPayload = [
             'update_id' => time(),
-            'edited_message' => $basicPayload,
+            'edited_message' => TelegramUpdateDtoMock::getDtoParams()['message'],
         ];
         unset($editPayload['edited_message']['text']);
 
+        $editPayload['edited_message']['photo'] = $photoData;
+
         $editPayload['edited_message']['caption'] = 'Новый текст сообщения';
         $editPayload['edited_message']['message_id'] = $newMessageData->from_id;
-        $editPayload['edited_message']['chat']['id'] = $botUser->chat_id;
-        $editPayload['edited_message']['message_thread_id'] = $botUser->topic_id;
+        $editPayload['edited_message']['chat']['id'] = $this->botUser->chat_id;
+        $editPayload['edited_message']['message_thread_id'] = $this->botUser->topic_id;
 
-        $newMessageDto = TelegramUpdateDto::fromRequest(
-            Request::create('api/telegram/bot', 'POST', $editPayload)
-        );
-        (new TgEditMessageService($newMessageDto))->handleUpdate();
-
-        $this->app->make('queue')->connection('sync');
+        $editMessageDto = TelegramUpdateDtoMock::getDto($editPayload);
+        (new TgEditMessageService($editMessageDto))->handleUpdate();
     }
 }
