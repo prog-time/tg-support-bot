827e96b96b8239cb84a5af88b75cfdf6fdaa1547
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-11-13 01:46:24 +0300

    поправил TgExternalMessageService
    ------------------------------
    Изменённые файлы:
    Изменён:    app/Services/TgExternal/TgExternalMessageService.php
    Изменён:    tests/Unit/Services/TgExternal/TgExternalMessageServiceTest.php

--- tests/Unit/Services/TgExternal/TgExternalMessageServiceTest.php
+++ tests/Unit/Services/TgExternal/TgExternalMessageServiceTest.php
@@ -2,8 +2,28 @@
 
 namespace Tests\Unit\Services\TgExternal;
 
+use App\Models\BotUser;
+use App\Models\Message;
+use App\Services\External\ExternalMessageService;
+use Tests\Mocks\External\ExternalMessageDtoMock;
 use Tests\TestCase;
 
 class TgExternalMessageServiceTest extends TestCase
 {
+    public function test_send_text_message(): void
+    {
+        Message::truncate();
+
+        $dto = ExternalMessageDtoMock::getDto();
+        $botUser = (new BotUser())->getExternalBotUser($dto);
+
+        (new ExternalMessageService($dto))->handleUpdate();
+
+        // Проверяем, что сообщение сохранилось в базе
+        $this->assertDatabaseHas('messages', [
+            'bot_user_id' => $botUser->id,
+            'message_type' => 'incoming',
+            'platform' => config('testing.external.source'),
+        ]);
+    }
 }
