1accaf8cda71a00485092346ec9d3a9079a5e56c
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-11-16 15:40:45 +0300

    поправил TgExternalEditServiceTest
    ------------------------------
    Изменённые файлы:
    Добавлен:   tests/Mocks/Tg/TelegramUpdateDto_GroupMock.php
    Изменён:    tests/Unit/Services/TgExternal/TgExternalEditServiceTest.php

---
+++ tests/Mocks/Tg/TelegramUpdateDto_GroupMock.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Mocks\Tg;
+
+use App\DTOs\TelegramUpdateDto;
+use Illuminate\Support\Facades\Request;
+
+class TelegramUpdateDto_GroupMock extends TelegramUpdateDto
+{
+    public static function getDtoParams(): array
+    {
+        return [
+            'update_id' => time(),
+            'message' => [
+                'message_id' => time(),
+                'from' => [
+                    'id' => config('testing.tg_private.chat_id'),
+                    'is_bot' => false,
+                    'first_name' => config('testing.tg_private.first_name'),
+                    'last_name' => config('testing.tg_private.last_name'),
+                    'username' => config('testing.tg_private.username'),
+                    'language_code' => 'ru',
+                ],
+                'chat' => [
+                    'id' => config('testing.tg_group.chat_id'),
+                    'title' => 'Prog-Time',
+                    'is_forum' => true,
+                    'type' => 'supergroup',
+                ],
+                'date' => time(),
+                'edit_date' => time(),
+                'text' => 'Тестовое сообщение',
+                'message_thread_id' => 0,
+            ],
+        ];
+    }
+
+    public static function getDto(array $dtoParams = []): TelegramUpdateDto
+    {
+        if (empty($dtoParams)) {
+            $dtoParams = self::getDtoParams();
+        }
+
+        $request = Request::create('api/telegram/bot', 'POST', $dtoParams);
+        return TelegramUpdateDto::fromRequest($request);
+    }
+}
--- tests/Unit/Services/TgExternal/TgExternalEditServiceTest.php
+++ tests/Unit/Services/TgExternal/TgExternalEditServiceTest.php
@@ -2,8 +2,80 @@
 
 namespace Tests\Unit\Services\TgExternal;
 
+use App\Models\BotUser;
+use App\Models\ExternalMessage;
+use App\Models\Message;
+use App\Services\External\ExternalMessageService;
+use App\Services\TgExternal\TgExternalEditService;
+use App\Services\TgExternal\TgExternalMessageService;
+use Tests\Mocks\External\ExternalMessageDtoMock;
+use Tests\Mocks\Tg\TelegramUpdateDto_GroupMock;
+use Tests\Mocks\Tg\TelegramUpdateDtoMock;
 use Tests\TestCase;
 
 class TgExternalEditServiceTest extends TestCase
 {
+    public function test_edit_text_message(): void
+    {
+        Message::truncate();
+        ExternalMessage::truncate();
+
+        // новое сообщение
+        $newMessageText = 'Сообщение от клиента';
+
+        $newMessageParamsDto = ExternalMessageDtoMock::getDtoParams();
+        $newMessageParamsDto['text'] = $newMessageText;
+
+        $newMessageDto = ExternalMessageDtoMock::getDto($newMessageParamsDto);
+
+        $botUser = (new BotUser())->getExternalBotUser($newMessageDto);
+
+        (new ExternalMessageService($newMessageDto))->handleUpdate();
+
+        // Проверяем, что сообщение сохранилось в базе
+        $whereMessageParams = [
+            'bot_user_id' => $botUser->id,
+            'message_type' => 'incoming',
+            'platform' => $botUser->platform,
+        ];
+        $this->assertDatabaseHas('messages', $whereMessageParams);
+
+        $newMessageData = Message::where($whereMessageParams)
+            ->orderBy('id', 'desc')
+            ->with('externalMessage')
+            ->first();
+
+        $this->assertNotNull($newMessageData->externalMessage);
+        // ---------------
+
+        // новое сообщение из группы
+        $newGroupMessageDto = TelegramUpdateDto_GroupMock::getDtoParams();
+        $newGroupMessageDto['message']['message_thread_id'] = $botUser->topic_id;
+        (new TgExternalMessageService(TelegramUpdateDto_GroupMock::getDto($newGroupMessageDto)))->handleUpdate();
+
+        // Проверяем, что сообщение сохранилось в базе
+        $whereMessageParams = [
+            'bot_user_id' => $botUser->id,
+            'message_type' => 'outgoing',
+            'platform' => $botUser->platform,
+        ];
+        $this->assertDatabaseHas('messages', $whereMessageParams);
+        // ---------------
+
+        // изменение сообщения
+        $newGroupMessageData = Message::where($whereMessageParams)->orderBy('id', 'desc')->first();
+
+        $editPayload = [
+            'update_id' => time(),
+            'edited_message' => TelegramUpdateDto_GroupMock::getDtoParams()['message'],
+        ];
+
+        $editPayload['edited_message']['text'] = 'Новый текст сообщения';
+        $editPayload['edited_message']['message_id'] = $newGroupMessageData->from_id;
+        $editPayload['edited_message']['message_thread_id'] = $botUser->topic_id;
+
+        $editMessageDto = TelegramUpdateDtoMock::getDto($editPayload);
+
+        (new TgExternalEditService($editMessageDto))->handleUpdate();
+    }
 }
