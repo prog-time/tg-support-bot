e8fcefb0844618812013825c8ea8960bdb7c0444
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-11-24 19:50:21 +0300

    поправил TgEditMessageServiceTest
    ------------------------------
    Изменённые файлы:
    Изменён:    tests/Unit/Services/Tg/TgEditMessageServiceTest.php

--- tests/Unit/Services/Tg/TgEditMessageServiceTest.php
+++ tests/Unit/Services/Tg/TgEditMessageServiceTest.php
@@ -7,15 +7,12 @@ use App\Models\BotUser;
 use App\Models\Message;
 use App\Services\Tg\TgEditMessageService;
 use App\Services\Tg\TgMessageService;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Queue;
 use Tests\Mocks\Tg\TelegramUpdateDtoMock;
 use Tests\TestCase;
 
 class TgEditMessageServiceTest extends TestCase
 {
-    use RefreshDatabase;
-
     private ?BotUser $botUser;
 
     private string $groupChatId;
@@ -83,87 +80,76 @@ class TgEditMessageServiceTest extends TestCase
         $this->assertEquals($editDto->text, $secondJob->queryParams->text);
     }
 
-    //
-    //    public function test_edit_caption_message(): void
-    //    {
-    //        $photo = [
-    //            [
-    //                'file_id'        => config('testing.tg_file.photo'),
-    //                'file_unique_id' => 'AQAD854DoEp9',
-    //                'file_size'      => 59609,
-    //                'width'          => 684,
-    //                'height'         => 777,
-    //            ],
-    //        ];
-    //
-    //
-    //        /*
-    //         * 1. Создаём сообщение с caption
-    //         */
-    //        $payload = TelegramUpdateDtoMock::getDtoParams();
-    //        unset($payload['message']['text']);
-    //
-    //        $payload['message']['photo'] = $photo;
-    //        $payload['message']['caption'] = 'Версия 1';
-    //
-    //        $newMessageDto = TelegramUpdateDtoMock::getDto($payload);
-    //        (new TgMessageService($newMessageDto))->handleUpdate();
-    //
-    //
-    //        // Проверка БД
-    //        $params = [
-    //            'bot_user_id' => $this->botUser->id,
-    //            'message_type' => 'incoming',
-    //            'platform' => 'telegram',
-    //        ];
-    //        $this->assertDatabaseHas('messages', $params);
-    //
-    //        $createdMessage = Message::where($params)->latest()->first();
-    //
-    //
-    //        /*
-    //         * 2. Первая джоба — создание
-    //         */
-    //        $pushed = Queue::pushedJobs()[SendTelegramMessageJob::class] ?? [];
-    //        $this->assertCount(1, $pushed);
-    //
-    //        $firstJob = array_shift($pushed)['job'];
-    //
-    //        $jobCaption = $firstJob->updateDto->message['caption'] ?? null;
-    //        $this->assertEquals('Версия 1', $jobCaption);
-    //
-    //
-    //        /*
-    //         * 3. Редактируем caption
-    //         */
-    //        $editPayload = [
-    //            'update_id'      => time(),
-    //            'edited_message' => TelegramUpdateDtoMock::getDtoParams()['message'],
-    //        ];
-    //
-    //        unset($editPayload['edited_message']['text']);
-    //
-    //        $editPayload['edited_message']['photo'] = $photo;
-    //        $editPayload['edited_message']['caption'] = 'Новый текст сообщения';
-    //
-    //        $editPayload['edited_message']['message_id'] = $createdMessage->from_id;
-    //        $editPayload['edited_message']['chat']['id'] = $this->botUser->chat_id;
-    //        $editPayload['edited_message']['message_thread_id'] = $this->botUser->topic_id;
-    //
-    //        $editDto = TelegramUpdateDtoMock::getDto($editPayload);
-    //        (new TgEditMessageService($editDto))->handleUpdate();
-    //
-    //
-    //        /*
-    //         * 4. Вторая джоба — редактирование caption
-    //         */
-    //        $pushed = Queue::pushedJobs()[SendTelegramMessageJob::class] ?? [];
-    //        $this->assertCount(1, $pushed);
-    //
-    //        $secondJob = $pushed[0]['job'];
-    //
-    //        $editedCaption = $secondJob->updateDto->edited_message['caption'] ?? null;
-    //
-    //        $this->assertEquals('Новый текст сообщения', $editedCaption);
-    //    }
+    public function test_edit_caption_message(): void
+    {
+        $photo = [
+            [
+                'file_id' => config('testing.tg_file.photo'),
+                'file_unique_id' => 'AQAD854DoEp9',
+                'file_size' => 59609,
+                'width' => 684,
+                'height' => 777,
+            ],
+        ];
+
+        // Создаём сообщение с caption
+        $payload = TelegramUpdateDtoMock::getDtoParams();
+        unset($payload['message']['text']);
+
+        $payload['message']['photo'] = $photo;
+        $payload['message']['caption'] = 'Версия 1';
+
+        $newMessageDto = TelegramUpdateDtoMock::getDto($payload);
+
+        (new TgMessageService($newMessageDto))->handleUpdate();
+
+        // Проверка БД
+        $whereMessageParams = [
+            'bot_user_id' => $this->botUser->id,
+            'message_type' => 'incoming',
+            'platform' => 'telegram',
+            'from_id' => rand(),
+            'to_id' => rand(),
+        ];
+        $createdMessage = Message::where($whereMessageParams)->firstOrCreate($whereMessageParams);
+
+        // Первая джоба — создание
+        /** @phpstan-ignore-next-line */
+        $pushed = Queue::pushedJobs()[SendTelegramMessageJob::class] ?? [];
+        $this->assertCount(1, $pushed);
+
+        $firstJob = array_shift($pushed)['job'];
+        $jobCaption = $firstJob->updateDto->caption ?? null;
+
+        $this->assertEquals('Версия 1', $jobCaption);
+
+        // Редактируем caption
+        $editPayload = [
+            'update_id' => time(),
+            'edited_message' => TelegramUpdateDtoMock::getDtoParams()['message'],
+        ];
+
+        unset($editPayload['edited_message']['text']);
+
+        $editPayload['edited_message']['photo'] = $photo;
+        $editPayload['edited_message']['caption'] = 'Новый текст сообщения';
+
+        $editPayload['edited_message']['message_id'] = $createdMessage->from_id;
+        $editPayload['edited_message']['chat']['id'] = $this->botUser->chat_id;
+        $editPayload['edited_message']['message_thread_id'] = $this->botUser->topic_id;
+
+        $editDto = TelegramUpdateDtoMock::getDto($editPayload);
+        (new TgEditMessageService($editDto))->handleUpdate();
+
+        // Вторая джоба — редактирование caption
+        /** @phpstan-ignore-next-line */
+        $pushed = Queue::pushedJobs()[SendTelegramMessageJob::class] ?? [];
+        $this->assertCount(2, $pushed);
+
+        $secondJob = $pushed[1]['job'];
+
+        $editedCaption = $secondJob->updateDto->caption ?? null;
+
+        $this->assertEquals('Новый текст сообщения', $editedCaption);
+    }
 }
