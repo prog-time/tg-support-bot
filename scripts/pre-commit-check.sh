d1bba0b9421df4c724c2e9cd8bf69ba4ca967a82
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2025-12-06 23:44:30 +0300

    set scripts by pre-commit
    ------------------------------
    Изменённые файлы:
    Изменён:    scripts/pre-commit-check.sh
    Изменён:    scripts/pre-push-check.sh

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -7,6 +7,11 @@ echo "🎨 Исправление стиля кода (Pint)..."
 bash scripts/check_scripts/check_pint.sh commit
 echo
 
+# проверка на наличие ошибок
+echo "🧪 Проверка типизации (PHPStan)..."
+bash scripts/check_scripts/check_phpstan.sh commit
+echo
+
 # проверка наличия тестов
 echo "🔍 Проверка наличия тестов..."
 bash scripts/check_scripts/find_test.sh commit
@@ -16,9 +21,3 @@ echo
 echo "🧑🏻‍💻 Проверка работы тестов..."
 bash scripts/check_scripts/ssh_start_tests.sh commit
 echo
-
-# проверка на наличие ошибок
-echo "🧪 Проверка типизации (PHPStan)..."
-bash scripts/check_scripts/check_phpstan.sh commit
-echo
-
--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 set -e
-
-echo "🧪 Генерация Swagger"
-bash scripts/check_scripts/check_swagger.sh commit
-echo
