a098217866e3aef31a113e39b38c88942c9b163a
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2026-01-08 15:40:37 +0300

    issues-62 | update GetFileTest
    ------------------------------
    Изменённые файлы:
    Изменён:    tests/Unit/Actions/Telegram/GetFileTest.php

--- tests/Unit/Actions/Telegram/GetFileTest.php
+++ tests/Unit/Actions/Telegram/GetFileTest.php
@@ -3,24 +3,39 @@
 namespace Tests\Unit\Actions\Telegram;
 
 use App\Actions\Telegram\GetFile;
+use App\DTOs\TelegramAnswerDto;
+use Illuminate\Support\Facades\Http;
+use Mockery;
 use Tests\TestCase;
 
 class GetFileTest extends TestCase
 {
-    private string $photoFileId;
-
-    public function setUp(): void
+    protected function tearDown(): void
     {
-        parent::setUp();
-
-        $this->photoFileId = config('testing.tg_file.photo');
+        Mockery::close();
+        parent::tearDown();
     }
 
-    public function test_get_file(): void
+    public function test_execute_returns_telegram_answer_dto(): void
     {
-        $result = GetFile::execute($this->photoFileId);
+        $fileId = 'abc123';
 
-        $this->assertTrue($result->ok);
-        $this->assertNotEmpty($result->rawData);
+        $data = [
+            'ok' => true,
+            'result' => [
+                'file_id' => $fileId,
+                'file_path' => 'path/to/file.jpg',
+            ],
+        ];
+        $expectedDto = TelegramAnswerDto::fromData($data);
+
+        Http::fake([
+            'https://api.telegram.org/*/getFile*' => Http::response($data, 200),
+        ]);
+
+        $result = GetFile::execute($fileId);
+
+        $this->assertInstanceOf(TelegramAnswerDto::class, $result);
+        $this->assertEquals($expectedDto->rawData['result']['file_path'], $result->rawData['result']['file_path']);
     }
 }
