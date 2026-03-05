3df8afb85f95bc4c1bdc1f06224a9390909e4f60
Илья Лящук <40496434+prog-time@users.noreply.github.com>
2026-03-05 16:31:35 +0300

    issues-99 | fixed api.php
    ------------------------------
    Files:
    Changed:  routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,11 @@
 <?php
 
-use App\Http\Controllers\ExternalTrafficController;
 use App\Http\Controllers\FilesController;
-use App\Middleware\ApiQuery;
 use Illuminate\Support\Facades\Route;
 
 // Telegram routes are registered by App\Modules\Telegram\TelegramServiceProvider
 // VK routes are registered by App\Modules\Vk\VkServiceProvider
-
-Route::group([
-    'prefix' => 'external',
-    'middleware' => ApiQuery::class,
-], function () {
-    Route::group([
-        'prefix' => '{external_id}',
-    ], function () {
-        Route::group([
-            'prefix' => 'messages',
-        ], function () {
-            Route::get('/{id_message}', [ExternalTrafficController::class, 'show'])->name('show');
-            Route::get('/', [ExternalTrafficController::class, 'index'])->name('index');
-            Route::post('/', [ExternalTrafficController::class, 'store'])->name('store');
-            Route::put('/', [ExternalTrafficController::class, 'update'])->name('update');
-            Route::delete('/', [ExternalTrafficController::class, 'destroy'])->name('destroy');
-        });
-
-        Route::group([
-            'prefix' => 'files',
-        ], function () {
-            Route::post('/', [ExternalTrafficController::class, 'sendFile'])->name('file_send');
-        });
-    });
-});
+// External routes are registered by App\Modules\External\ExternalServiceProvider
 
 Route::group([
     'prefix' => 'files',
